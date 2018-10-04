542d75ee3045837b5bba61f72198bfe7ff1c94dc
TejaswitaW <TejaswitaWakhure@gmail.com>
2018-10-04 22:19:16 +0530

    Find number of occurrences of each character in string

--- string5.py
+++ string5.py
@@ -1,9 +1,11 @@
 #Find number of occurrences of each character in string
 s="ABCABCABABAB"
 d={}
+# uses character as key
+#number of occurences as values
 for x in s:
     if x not in d.keys():
-        d[x]=1
+        d[x]=1 
     else:
         d[x]=d[x]+1
 for k,v in d.items():
