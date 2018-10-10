35dd3e2dd03c2ee5352a5ebb36d3ca0f8fbdcfeb
TejaswitaW <TejaswitaWakhure@gmail.com>
2018-10-10 18:19:13 +0530

    finding vowels using set

---
+++ SetFun.py
@@ -0,0 +1,5 @@
+#finding vowels using set
+s="My name is Tejaswita"
+s1=set(s)
+s2={"a","e","i","o","u"}
+print("Vowels in the given string are :",s1.intersection(s2))
--- SetFunctions.py
+++ SetFunctions.py
@@ -6,3 +6,23 @@ print(s)
 l=[10,20,30,40,50]
 s.update(l)
 print(s)
+s.update("kh"+"df")
+print(s)
+s1=s
+print(s1)
+print(s.pop())
+print(s)
+print(s1)
+s.remove(50)
+print(s)
+print(s1)
+print(s1.intersection(s))
+print(s1.union(s))
+print(s1.symmetric_difference(s))
+t=set("Tejaswita")
+print("t" in t)
+x={0,1,2,3,4,5,6}
+y={i*i*i for i in x}
+print(y)
+z={i for i in x if i%2==0}
+print(z)
